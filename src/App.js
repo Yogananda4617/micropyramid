fb3918b0b47ac55ee1009cc1eaebbf31894ed100
Yogananda Potnuru <85452073+Yogananda4617@users.noreply.github.com>
2022-06-24 23:30:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import Register from './register/register';
 import Home from './home/home';
 
 function App() {
-  const user = localStorage.getItem("token")
+  const user = localStorage.getItem("username")
   return (
     <div className="App">
       <Router>
-      {user && <Route path="/home" ><Home exact /> </Route>}
+      {user && <Route path="/home" ><Home/> </Route>}
 			<Route exact path="/register"><Register/></Route> 
 			<Route exact path="/login"> <Login /> </Route> 
 			<Route path="/" > <Redirect to="/login" /> </Route>
